1c1c4747c74e7bd44cc5ca269a86c1323f905202
kir_rik <kir-rik-90@yandex.ru>
2020-08-11 22:45:53 +0400

    ignore

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
-var varIsBad = 1
+const varIsBad = 1
 
 const underscoreCaseIsBad = '1'
 
 const constantsArePreferable = null
 
+// eslint-disable-next-line max-len
 console.log(varIsBad, underscoreCaseIsBad, constantsArePreferable)
 
 const str = 'http://www.google.com?q=12345678901234567890123456789012345678901234567890'
